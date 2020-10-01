3d50b671a5425d2df5341d14c53e01bb20dae527
Meelad <meladmansoori@gmail.com>
2020-10-01 11:30:36 +0330

    first commit

---
+++ page-objects/googleAdvancedSearch.js
@@ -0,0 +1,22 @@
+module.exports = {
+  url: 'https://www.google.com/advanced_search',
+  elements: {
+    mainQueryInput: 'input[name="as_q"]',
+    languageDropdown: '#lr_button',
+    lastUpdateDropdown: '#as_qdr_button',
+    submitButton: '.jfk-button[type="submit"]',
+  },
+  commands: [
+    {
+      setQuery(value) {
+        return this.setValue('@mainQueryInput', value)
+      },
+      selectFilter(selector, value) {
+        return this.click(selector).click(`.goog-menuitem[value="${value}"]`)
+      },
+      submitSearch() {
+        return this.click('@submitButton')
+      },
+    },
+  ],
+}
