ad75e4955aa185b3d70ee51158e8b2e117d8090d
Meelad <meladmansoori@gmail.com>
2020-10-03 11:51:42 +0330

    cucumber added to the project

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,30 +1,43 @@
+// module.exports = {
+//   src_folders: ['tests'],
+//   // page_objects_path: ['page-objects'],
+//   webdriver: {
+//     start_process: true,
+//     server_path: require('chromedriver').path,
+//     port: 9515,
+//   },
+//   test_settings: {
+//     default: {
+//       screenshots: {
+//         enabled: true,
+//         on_failure: true,
+//         on_error: true,
+//         path: 'tests_output/screenshots',
+//       },
+//       desiredCapabilities: {
+//         browserName: 'chrome',
+//         chromeOptions: {
+//           args: ['--headless'],
+//         },
+//       },
+//     },
+//   },
+// }
+
+const chromedriver = require('chromedriver')
+
 module.exports = {
-  src_folders: ['tests'],
-  page_objects_path: ['page-objects'],
-  // exclude: ['tests/globals.js'],
-  webdriver: {
-    start_process: true,
-    server_path: require('chromedriver').path,
-    port: 9515,
-  },
   test_settings: {
     default: {
-      screenshots: {
-        enabled: true,
-        on_failure: true,
-        on_error: true,
-        path: 'tests_output/screenshots',
+      webdriver: {
+        start_process: true,
+        server_path: chromedriver.path,
+        port: 4444,
+        cli_args: ['--port=4444'],
       },
       desiredCapabilities: {
         browserName: 'chrome',
-        chromeOptions: {
-          args: ['--headless'],
-        },
       },
-      // globals: {
-      //   asyncHookTimeout: 60000,
-      // },
     },
   },
-  // globals_path: 'tests/globals.js',
 }
